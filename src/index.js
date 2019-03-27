92a074d1c9baba7b6bfe63a9cf6e458267672e63
Muhammad Shafiq <Muhammad.shafiq@bentely.com>
2019-03-27 16:37:41 +0500

    added two reducers userReducer and productReducer using combineReducer

--- src/index.js
+++ src/index.js
@@ -3,44 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-const changeState = 'changeState';
-const oneMoreActionConst = 'oneMoreAction';
-const action = {
-    type: changeState,
-    payload: {
-        newState: 'updated state'
-    }
-}
-const oneMoreAction = {
-    type: oneMoreActionConst,
-    payload: {
-        newState: 'one more state update'
-    }
-}
-let store = createStore(reducer);
-
-function reducer(state, action) {
-    switch (action.type) {
-        case changeState:
-            return action.payload.newState;
-        case oneMoreActionConst:
-            return action.payload.newState;
-
-        default:
-            return "state";
-    }
+function productReducer(state = [], action) {
+    return state;
 }
 
-console.log(store.getState());
-store.dispatch(action);
-console.log(store.getState());
-store.dispatch(oneMoreAction);
+function userReducer(state = [], action) {
+    return state;
+}
+
+const allReducers = combineReducers({ products: productReducer, users: userReducer })
+
+let store = createStore(allReducers);
+
 console.log(store.getState());
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
