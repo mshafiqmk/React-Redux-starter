cf9f05fe131acc89abb9a51e1fdec7e5306f367a
Muhammad Shafiq <Muhammad.shafiq@bentely.com>
2019-03-27 16:25:36 +0500

    intial commit with simple use of redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+const changeState = 'changeState';
+const oneMoreActionConst = 'oneMoreAction';
+const action = {
+    type: changeState,
+    payload: {
+        newState: 'updated state'
+    }
+}
+const oneMoreAction = {
+    type: oneMoreActionConst,
+    payload: {
+        newState: 'one more state update'
+    }
+}
+let store = createStore(reducer);
+
+function reducer(state, action) {
+    switch (action.type) {
+        case changeState:
+            return action.payload.newState;
+        case oneMoreActionConst:
+            return action.payload.newState;
+
+        default:
+            return "state";
+    }
+}
+
+console.log(store.getState());
+store.dispatch(action);
+console.log(store.getState());
+store.dispatch(oneMoreAction);
+console.log(store.getState());
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
