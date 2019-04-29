3435994538e4b1d63d93c0f468d33c255c18afb6
Muhammad Shafiq <Muhammad.shafiq@bentely.com>
2019-04-29 16:34:37 +0500

    update the .gitignore file

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
-import { updateUser, addUser } from '../actions/userActions'
+import React, { Component } from "react";
+import "./App.css";
+import { updateUser, addUser } from "../actions/userActions";
 import { connect } from "react-redux";
 
 class App extends Component {
   handleUpdateUser = () => {
     this.props.updateuserprop({ userid: 2, userName: "khuidad" });
-  }
+  };
   handleAddUser = () => {
-    this.props.adduserprop({ userid: 3, userName: 'ADNANA' })
-  }
+    this.props.adduserprop({ userid: 3, userName: "ADNANA" });
+  };
   render() {
-    console.log("props = ", this.props)
+    console.log("props = ", this.props);
     return (
       <div className="App">
-        <div onClick={this.handleUpdateUser} >
-          update user
-        </div>
-        <div onClick={this.handleAddUser}>
-          add user
-        </div>
+        <div onClick={this.handleUpdateUser}>update user</div>
+        <div onClick={this.handleAddUser}>add user</div>
         <p>update for ci</p>
       </div>
     );
@@ -29,11 +25,14 @@ class App extends Component {
 const mapStateToProps = state => ({
   users: state.users,
   products: state.products
-})
+});
 
 const mapActionToProps = {
   updateuserprop: updateUser,
   adduserprop: addUser
-}
+};
 
-export default connect(mapStateToProps, mapActionToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapActionToProps
+)(App);
